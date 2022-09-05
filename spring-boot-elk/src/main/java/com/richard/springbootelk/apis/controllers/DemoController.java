2043d95f3f03a8693a58f2976d270213811389ed
Richard Vu <richardvu.work@gmail.com>
2022-09-05 16:59:47 +0700

    spring-boot-elk

---
+++ spring-boot-elk/src/main/java/com/richard/springbootelk/apis/controllers/DemoController.java
@@ -0,0 +1,42 @@
+package com.richard.springbootelk.apis.controllers;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class DemoController {
+  
+  @GetMapping("/")
+    public String HomePage(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.info("Welcome home Page " + localDateTime);
+        return "Welcome to Home page";
+    }
+
+    @GetMapping("/logs")
+    public String LogsPage(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.info("This Logs page " + localDateTime);
+        return "Welcome to logs page";
+    }
+
+    @GetMapping("/warn")
+    public String WarnPage(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.warn("This warn page " + localDateTime);
+        return "Welcome to warn page";
+    }
+
+    @GetMapping("/er")
+    public String ErrorPage(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.error("This error page " + localDateTime);
+        return "Welcome to error page";
+    }
+    
+}
